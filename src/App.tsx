cacab867dba592ffe131f9b1d6154660bcfccdc9
Gabriel <dev.gabrielramos@gmail.com>
2021-09-28 15:46:54 -0300

    Update App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
-import logo from './logo.svg'
-import './App.css'
+import { useState } from 'react'
+
+import { Header } from 'components/Header'
+import { Main } from 'components/Main'
+import { NewTransactionModal } from 'components/NewTransactionModal'
+
+import { ThemeProvider } from 'styled-components/macro'
+import { GlobalStyle } from 'styles/global'
+import { theme } from 'styles/theme'
 
 function App () {
+  const [isTheNewTransactionModalOpen, setIsTheNewTransactionModalOpen] =
+    useState(false)
+
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Header setIsTheNewTransactionModalOpen={setIsTheNewTransactionModalOpen} />
+
+      <Main />
+      <NewTransactionModal
+        isTheNewTransactionModalOpen={isTheNewTransactionModalOpen}
+        setIsTheNewTransactionModalOpen={setIsTheNewTransactionModalOpen}
+      />
+    </ThemeProvider>
   )
 }
 
