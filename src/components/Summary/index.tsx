8bf1b20f87e45e5381364f7c0eb98eafe2a34ee2
Gabriel <dev.gabrielramos@gmail.com>
2021-09-28 15:48:43 -0300

    Create Summary component

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,38 @@
+import { Container, Card, FullSummaryCard, Header, Type, Amount } from './styles'
+
+import incomeIconPath from 'assets/income.svg'
+import outcomeIconPath from 'assets/outcome.svg'
+import totalIconPath from 'assets/total.svg'
+
+export function Summary () {
+  return (
+    <Container>
+      <Card>
+        <Header>
+          <Type>Entradas</Type>
+          <img src={incomeIconPath} alt='Entradas' />
+        </Header>
+
+        <Amount>R$ 17.400,00</Amount>
+      </Card>
+
+      <Card>
+        <Header>
+          <Type>Saídas</Type>
+          <img src={outcomeIconPath} alt='Saídas' />
+        </Header>
+
+        <Amount>R$ 1.259,00</Amount>
+      </Card>
+
+      <FullSummaryCard isItAPositiveTotal>
+        <Header>
+          <Type>Total</Type>
+          <img src={totalIconPath} alt='Total' />
+        </Header>
+
+        <Amount>R$ 16.141,00</Amount>
+      </FullSummaryCard>
+    </Container>
+  )
+}
---
+++ src/components/Summary/styles.ts
@@ -0,0 +1,48 @@
+import styled from 'styled-components/macro'
+
+type FullSummaryCardProps = {
+  isItAPositiveTotal?: boolean
+}
+
+export const Container = styled.section`
+  display: flex;
+  justify-content: space-between;
+  gap: 2rem;
+`
+
+export const Card = styled.div`
+  width: 22rem;
+  padding: 1.574375rem 1.291875rem 1.133125rem 2rem;
+  border-radius: 0.3125rem;
+  color: ${props => props.theme.colors.title};
+  background-color: ${props => props.theme.colors.white};
+`
+
+export const FullSummaryCard = styled(Card)<FullSummaryCardProps>`
+  color: ${props => props.theme.colors.white};
+  background-color: ${props => props.isItAPositiveTotal
+    ? props.theme.colors.green
+    : props.theme.colors.red
+  };
+`
+
+export const Header = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 0.633125rem;
+`
+
+export const Amount = styled.strong`
+  font-weight: 500;
+  font-size: 2rem;
+  line-height: 3.375rem;
+  color: inherit;
+`
+
+export const Type = styled.h2`
+  font-weight: 400;
+  font-size: 1rem;
+  line-height: 1.5rem;
+  color: inherit;
+`
