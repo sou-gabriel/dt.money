7f5cb0382de8b35b4cd7dc7f5732fb1515862300
Gabriel <dev.gabrielramos@gmail.com>
2021-09-30 15:05:43 -0300

    Persist transactions on localforage

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
-import { useState } from 'react'
+import { useState, useEffect, useContext } from 'react'
+import localforage from 'localforage'
+import { TransactionsContext } from 'TransactionsContext'
 
 import { Header } from 'components/Header'
 import { Main } from 'components/Main'
@@ -8,15 +10,42 @@ import { ThemeProvider } from 'styled-components/macro'
 import { GlobalStyle } from 'styles/global'
 import { theme } from 'styles/theme'
 
+type Transaction = {
+  id: string
+  title: string
+  type: 'deposit' | 'withdraw'
+  price: number
+  category: string
+  createdAt: Date
+}
+
 export const App = () => {
   const [isTheNewTransactionModalOpen, setIsTheNewTransactionModalOpen] =
     useState(false)
+  // Persistir transações
+  const { transactions, setTransactions } = useContext(TransactionsContext)
+
+  useEffect(() => {
+    localforage.setItem('transactions', transactions)
+  }, [transactions])
+
+  useEffect(() => {
+    const updateStateWithLocalforageTransactions = async () => {
+      const transactionsFromLocalforage = await localforage
+        .getItem<Promise<Transaction[]>>('transactions')
+
+      if (transactionsFromLocalforage) {
+        setTransactions(transactionsFromLocalforage)
+      }
+    }
+
+    updateStateWithLocalforageTransactions()
+  }, [setTransactions])
 
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <Header setIsTheNewTransactionModalOpen={setIsTheNewTransactionModalOpen} />
-
       <Main />
       <NewTransactionModal
         isTheNewTransactionModalOpen={isTheNewTransactionModalOpen}
